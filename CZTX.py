cdf41df0820f290543d1da613ef3285b9b6be0b9
ARj-cyber <arjunpdinesh123@gmail.com>
2020-09-28 17:53:27 +0530

    main

---
+++ CZTX.py
@@ -0,0 +1,84 @@
+#! /usr/env/bin python3
+
+
+import os
+
+def menu():
+print("
+
+
+
+        CCCCCCCCCCCCCZZZZZZZZZZZZZZZZZZZTTTTTTTTTTTTTTTTTTTTTTTXXXXXXX       XXXXXXX
+     CCC::::::::::::CZ:::::::::::::::::ZT:::::::::::::::::::::TX:::::X       X:::::X
+   CC:::::::::::::::CZ:::::::::::::::::ZT:::::::::::::::::::::TX:::::X       X:::::X
+  C:::::CCCCCCCC::::CZ:::ZZZZZZZZ:::::Z T:::::TT:::::::TT:::::TX::::::X     X::::::X
+ C:::::C       CCCCCCZZZZZ     Z:::::Z  TTTTTT  T:::::T  TTTTTTXXX:::::X   X:::::XXX
+C:::::C                      Z:::::Z            T:::::T           X:::::X X:::::X
+C:::::C                     Z:::::Z             T:::::T            X:::::X:::::X
+C:::::C                    Z:::::Z              T:::::T             X:::::::::X
+C:::::C                   Z:::::Z               T:::::T             X:::::::::X
+C:::::C                  Z:::::Z                T:::::T            X:::::X:::::X
+C:::::C                 Z:::::Z                 T:::::T           X:::::X X:::::X
+ C:::::C       CCCCCCZZZ:::::Z     ZZZZZ        T:::::T        XXX:::::X   X:::::XXX
+  C:::::CCCCCCCC::::CZ::::::ZZZZZZZZ:::Z      TT:::::::TT      X::::::X     X::::::X
+   CC:::::::::::::::CZ:::::::::::::::::Z      T:::::::::T      X:::::X       X:::::X
+     CCC::::::::::::CZ:::::::::::::::::Z      T:::::::::T      X:::::X       X:::::X
+        CCCCCCCCCCCCCZZZZZZZZZZZZZZZZZZZ      TTTTTTTTTTT      XXXXXXX       XXXXXXX
+
+
+
+
+
+")
+print("=============================================")
+print("ARj-cyber again ;)")
+print("=============================================")
+
+
+print("
+1 . Update your termux
+2 . Customize homescreen with your own ASCII art
+3 . Contribute
+4 . Help
+")
+
+loop = True
+while loop:
+      menu()
+    inpu = input(">>>: ")
+
+
+ if inpu == "1":
+        print("Are you sure to  update your termux(y/n)")
+update=input(">>> :  ")
+
+
+
+if update == y
+os.system("apt update && apt upgrade -y")
+os.system("apt install -y git python python2 wget")
+print("======================")
+print("Operation successful ")
+print("======================")
+else :
+print("======================")
+print("Operation cancelled")
+print("======================")
+
+
+elif inpu == 2
+os.import(cd $HOME)
+os.import(cd ..)
+os.import(cd usr)
+os.import(cd etc)
+print('''
+>>>Before you proceed:
+        •You can use your own ASCII text for setting as
+welcome screen
+        •For your own ASCII text enter y
+        •For default enter n
+''')
+print("# Can we proceed now ? (y/n)")
+nano = input("[+]  ")
+if nano == y
+      os.import(
